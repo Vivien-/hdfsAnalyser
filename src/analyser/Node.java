17a35ea508d2573758c6777f769ac48e7beb5041
melmoumni <elmoumni.mohammed1@gmail.com>
2016-04-26 11:10:20 +0200

    Tree and Node structure (Optimization idea)

---
+++ src/analyser/Node.java
@@ -0,0 +1,88 @@
+package analyser;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
+public class Node{
+	
+	
+    private String data;
+    private long size;
+    private Timestamp lastModified;
+	private ArrayList<Node> children;
+
+	
+	
+    public Node(String data, long size, long lastModified){
+        this.data = data;
+        this.size = size;
+        this.lastModified = new Timestamp(lastModified);
+        children = new ArrayList<Node>();
+    }
+
+    
+    
+    
+    public String getData() {
+		return data;
+	}
+
+
+
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+
+
+
+	public long getSize() {
+		return size;
+	}
+
+
+
+
+	public void setSize(long size) {
+		this.size = size;
+	}
+
+
+
+
+	public ArrayList<Node> getChildren() {
+		return children;
+	}
+
+
+
+
+	public void setChildren(ArrayList<Node> children) {
+		this.children = children;
+	}
+
+
+
+
+	public Node getChild(String data){
+        for(Node n : children)
+            if(n.data.equals(data))
+                return n;
+
+        return null;
+    }
+	
+    
+    public long getLastModified() {
+		return lastModified.getTime();
+	}
+
+
+
+
+	public void setLastModified(long lastModified) {
+		this.lastModified.setTime(lastModified);
+	}
+
+}
